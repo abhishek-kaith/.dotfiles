bd67c5b1e0de5b8eb0d4f947283e8fb03dd6c632
Abhishek Kaith <abhishekkaith76@gmail.com>
2023-07-31 11:15:02 +0530

    codeium

--- nvim/.config/nvim/lua/kai/lazy.lua
+++ nvim/.config/nvim/lua/kai/lazy.lua
@@ -45,7 +45,7 @@ require("lazy").setup({
 			"jose-elias-alvarez/null-ls.nvim",
 			{
 				"j-hui/fidget.nvim",
-                tag="legacy",
+				tag = "legacy",
 				config = function()
 					require("fidget").setup()
 				end,
@@ -70,4 +70,21 @@ require("lazy").setup({
 	"nvim-tree/nvim-web-devicons",
 
 	"folke/zen-mode.nvim",
+
+	-- codeium ai code
+	"Exafunction/codeium.vim",
+	config = function()
+		vim.keymap.set("i", "<C-g>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true })
+		vim.keymap.set("i", "<c-;>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true })
+		vim.keymap.set("i", "<c-,>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true })
+		vim.keymap.set("i", "<c-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true })
+	end,
 })
